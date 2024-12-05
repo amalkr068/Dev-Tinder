e019a5319c053478fbb1d8470790be9fe4c41837
amalkr068 <amalkr068@gmail.com>
2024-12-05 00:55:56 -0800

    Dummy api's created ,and also test with postman

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ const express = require("express")
 const app = express()
 
 
-
-app.use('/test',(req,res)=>{
-    res.end("This is Test page")
+app.get("/user",(req,res)=>{
+    res.send({name:"Amal",age:23,place:"chy"})
 })
 
-app.use('/hello',(req,res)=>{
-    res.end("This is Hello page")
+app.post('/user',(req,res)=>{
+    res.send("data sent successfully")
 })
+
+app.delete('/user',(req,res)=>{
+    res.send("Data deleted successfully")
+})
+
+app.patch('/user',(req,res)=>{
+    res.send("Data updated successfully")
+})
+
 
 app.listen(3000,()=>{
     console.log("Server started on 3000 Port")
