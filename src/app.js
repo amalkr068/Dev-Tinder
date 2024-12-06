6e0be3d0cb7d8b9adb612ac728d1611081b089dc
amalkr068 <amalkr068@gmail.com>
2024-12-06 04:07:36 -0800

    basic CRUD done

--- src/app.js
+++ src/app.js
@@ -3,18 +3,11 @@ const app = express()
 const { connectDB } = require('./config/database')
 const {User} = require('./models/user')
 
-
-
+app.use(express.json())
 
 app.post('/signup',async (req,res)=>{
-    const newUser = new User({
-        firstName:"sangeeth",
-        lastName:'kr',
-        emailId:"sangeeth@gmail.com",
-        password:"sangeeth@123",
-        gender:"male",
-        age:26
-    })
+    
+   const newUser = new User(req.body)
 
     try{
         await newUser.save()
@@ -26,7 +19,64 @@ app.post('/signup',async (req,res)=>{
 })
 
 
+app.get('/user',async (req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = User.find({emailId:userEmail})
+        if(users.length === 0){
+            res.status(404).send("User not found")
+        } else {
+            res.send(users)
+        }
+    }
+    catch(err){
+        res.status(404).send("Something went wrong......!!!!")
+    }
+    
+  
+})
 
+app.get('/feed',async (req,res)=>{
+    try{
+        const users = await User.find({})
+        if(!users){
+            res.status(404).send("users not found.......!!!!!")
+        } else {
+            res.send(users)
+        }
+    }
+    catch (err){
+        res.status(404).send("Something went wrong......!!!!")
+    }
+  
+  
+})
+
+app.delete('/user',async (req,res)=>{
+
+    const userId = req.body.userId
+    try{
+        const users = await User.findByIdAndDelete(userId)
+        res.send("Data deleted successfully....!!!")
+    }
+    catch (err){
+        res.status(404).send("Something went wrong......!!!!")
+    }
+ 
+})
+
+app.patch('/user',async (req,res)=>{
+    const userId = req.body.userId
+    const data = req.body
+try{
+    const users =await User.findByIdAndUpdate(userId,data)
+    res.send("Updated successfully.....!!!!")
+}
+catch (err){
+    res.status(404).send("Something went wrong......!!!!")
+}
+   
+})
 
 connectDB().then(()=>{
     console.log("DB connected successfully")
